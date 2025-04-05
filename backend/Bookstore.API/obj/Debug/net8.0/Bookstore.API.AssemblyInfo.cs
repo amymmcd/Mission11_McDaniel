aff3eac1b1c529803166de2f1a1c82c49a59f6ca
amymmcd <amymargaret22@gmail.com>
2025-04-04 23:25:40 -0600

    delete publish folder

--- backend/Bookstore.API/obj/Debug/net8.0/Bookstore.API.AssemblyInfo.cs
+++ backend/Bookstore.API/obj/Debug/net8.0/Bookstore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab52c3a4cfc87c6e94dc46198fc28a5a54a4d59b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28cbb16b35a4bafe41acaa78470ae855477665b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

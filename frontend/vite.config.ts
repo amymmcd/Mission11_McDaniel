76c08b767d31e3c15564b625068efcb641e8b60e
amymmcd <amymargaret22@gmail.com>
2025-04-04 22:45:41 -0600

    update in vite.config.ts to help with deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // ✅ Important for deployment!
   plugins: [react()],
   server: {
     port: 3000,
